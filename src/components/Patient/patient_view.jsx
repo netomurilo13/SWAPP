3dc260b7ae8f19743ab2ead13fb77ca2158e8af8
netomurilo13 <netomurilo100@gmail.com>
2023-06-22 12:41:41 -0300

    visual identity - Galileu

--- src/components/Patient/patient_view.jsx
+++ src/components/Patient/patient_view.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from "react";
-import Sidebar from "../sidebar";
 import "./../../index.css";
-import logo from './../../assets/marisa.png';
+import logo from './../../assets/galileu_azul.png';
 
 const PatientList = () => {
-
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!isSidebarOpen);
-  };
-
   const [isIframeVisible, setIframeVisible] = useState(false);
 
   const handleExternalLink = () => {
@@ -19,12 +11,24 @@ const PatientList = () => {
 
   return (
     <div className="App">
-      <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="content">
-        <img src={logo} alt="Logo" className="logo" />
+        <img src={logo} alt="Logo" className="logo" style={{ marginBottom: "100px" }} />
+        <div className="text-container">
+          <p className="main-text">
+            Todas as informações que você ceder para mim serão usadas para fornecer uma melhor consulta médica para você!
+            Ao clicar para abrir o formulário você concorda com a nossa{" "}
+            <a href="https://i9.med.br/termos-de-uso-da-plataforma-i9med-saude-digital/" target="_blank" rel="noopener noreferrer">
+              política de proteção de dados e privacidade
+            </a>.
+            Para acessar o formulário, clique no botão abaixo:
+          </p>
+          <div className="form-button-container">
+            <button className="form-button" onClick={handleExternalLink}>
+              Abrir Formulário
+            </button>
+          </div>
         </div>
-         <button onClick={handleExternalLink}>Abrir Formulário</button>
-
+      </div>
       {isIframeVisible && (
         <iframe
           src="https://www.appsheet.com/start/ed2be4b3-ca16-434b-a258-0c84007a65bf#appName=MarisaCare-04-640720421&page=form&row=&table=P%C3%A1gina1&view=Formul%C3%A1rio"
@@ -40,7 +44,7 @@ const PatientList = () => {
           }}
         />
       )}
-      </div>
+    </div>
   );
 };
 
