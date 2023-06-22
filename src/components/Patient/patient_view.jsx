2f632f7b0a13d84cf6008d82126cb07cfb8875c2
netomurilo13 <netomurilo100@gmail.com>
2023-06-22 10:56:15 -0300

    clean application and input form

--- src/components/Patient/patient_view.jsx
+++ src/components/Patient/patient_view.jsx
@@ -6,72 +6,17 @@ import "./../../index.css";
 import logo from './../../assets/marisa.png';
 
 const PatientList = () => {
-  const patients = [
-    {
-      name: "Patient 1",
-      day: new Date(2023, 5, 10),
-      time: "10:00 AM",
-      details: "Some details about Patient 1",
-    },
-    {
-      name: "Patient 2",
-      day: new Date(2023, 5, 11),
-      time: "11:30 AM",
-      details: "Some details about Patient 2",
-    },
-    {
-      name: "Patient 3",
-      day: new Date(2023, 6, 10),
-      time: "2:00 PM",
-      details: "Some details about Patient 3",
-    },
-    {
-      name: "Patient 4",
-      day: new Date(2023, 11, 12),
-      time: "9:45 AM",
-      details: "Some details about Patient 4",
-    },
-    {
-      name: "Patient 5",
-      day: new Date(2023, 5, 13),
-      time: "3:15 PM",
-      details: "Some details about Patient 5",
-    },
-  ];
+
 
   const groupedPatients = {};
 
-  // Agrupar pacientes por dia
-  patients.forEach((patient) => {
-    const day = patient.day.toDateString();
-    if (!groupedPatients[day]) {
-      groupedPatients[day] = [];
-    }
-    groupedPatients[day].push(patient);
-  });
-
   const [isSidebarOpen, setSidebarOpen] = useState(false);
-  const [selectedDate, setSelectedDate] = useState(new Date());
-  const [selectedPatient, setSelectedPatient] = useState(null);
+
+
 
   const toggleSidebar = () => {
     setSidebarOpen(!isSidebarOpen);
   };
-
-  const handleDateChange = (date) => {
-    setSelectedDate(date);
-  };
-
-  const handlePatientClick = (patient) => {
-    setSelectedPatient(patient);
-  };
-
-  const handleClosePopup = () => {
-    setSelectedPatient(null);
-  };
-
-  // Filtrar pacientes por data completa
-  const filteredPatients = groupedPatients[selectedDate.toDateString()] || [];
 
   const [isIframeVisible, setIframeVisible] = useState(false);
 
@@ -84,25 +29,8 @@ const PatientList = () => {
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <div className="content">
         <img src={logo} alt="Logo" className="logo" />
-        <h1>Lista de Pacientes por Dia</h1>
-        <div className="calendar-container">
-          <Calendar onChange={handleDateChange} value={selectedDate} />
         </div>
-        <div className="patients-list">
-          <h2>{selectedDate.toDateString()}</h2>
-          <ul>
-            {filteredPatients.map((patient, index) => (
-              <li
-                key={index}
-                className="patient-item"
-                onClick={() => handlePatientClick(patient)}
-              >
-                {patient.name}
-              </li>
-            ))}
-          </ul>
-        </div>
-         <button onClick={handleExternalLink}>Abrir App Externo</button>
+         <button onClick={handleExternalLink}>Abrir Formulário</button>
 
       {isIframeVisible && (
         <iframe
@@ -120,19 +48,6 @@ const PatientList = () => {
         />
       )}
       </div>
-      {selectedPatient && (
-        <div className="popup-overlay">
-          <div className="popup">
-            <h3>Detalhes do Patient:</h3>
-            <p>Nome: {selectedPatient.name}</p>
-            <p>Dia: {selectedPatient.day.toDateString()}</p>
-            <p>Hora: {selectedPatient.time}</p>
-            <p>Detalhes: {selectedPatient.details}</p>
-            <button onClick={handleClosePopup}>Fechar</button>
-          </div>
-        </div>
-      )}
-    </div>
   );
 };
 
