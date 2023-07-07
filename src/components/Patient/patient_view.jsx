f3c7568be325c7755e9ead99cf3dc10d55e58348
netomurilo13 <netomurilo100@gmail.com>
2023-07-07 13:10:44 -0300

    add group

--- src/components/Patient/patient_view.jsx
+++ src/components/Patient/patient_view.jsx
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./../../index.css";
 import logo from './../../assets/galileu_azul.png';
 import TagManager from 'react-gtm-module';
+import { useLocation } from "react-router-dom";
 
 const PatientList = () => {
   const [isIframeVisible, setIframeVisible] = useState(false);
   const [isButtonClicked, setButtonClicked] = useState(false);
+  const location = useLocation();
+  const parameter = new URLSearchParams(location.search).get("parameter");
 
   useEffect(() => {
     const tagManagerArgs = {
@@ -19,8 +22,25 @@ const PatientList = () => {
     setButtonClicked(true);
   };
 
-  return (
-    <div className="App">
+  const encodeParameter = encodeURIComponent(parameter);
+  const taruma = "%7B%0A%22group%22%20%3A%20%22Tarum%C3%A3%22%0A%7D";
+  const i9med = "%7B%0A%22group%22%20%3A%20%22i9med%22%0A%7D";
+  const conveniados = "%7B%0A%22group%22%20%3A%20%22Piloto%20Galileu%C2%A0CEJAM%22%0A%7D";
+  console.log(taruma === encodeParameter)
+
+
+  let errorMessage = null;
+  let content = null;
+
+  if (encodeParameter !== taruma && encodeParameter !== conveniados && encodeParameter !== i9med) {
+    errorMessage = (
+      <div className="error-container">
+        <img src={logo} alt="Logo" className="logo" style={{ marginBottom: "100px" }} />
+        <p className="error-message">Link inválido. Favor solicitar novamente o link de acesso a essa aplicação.</p>
+      </div>
+    );
+  } else {
+    content = (
       <div className="content">
         <img src={logo} alt="Logo" className="logo" style={{ marginBottom: "100px" }} />
         <div className="text-container">
@@ -40,9 +60,16 @@ const PatientList = () => {
           </div>
         </div>
       </div>
+    );
+  }
+
+  return (
+    <div className="App">
+      {errorMessage || content}
       {isIframeVisible && (
         <iframe
-          src={`https://www.appsheet.com/start/e81f77d3-5b6c-4408-a07e-ae254943a988#appName=Galileu_backup-640720421&page=form&row=&table=P%C3%A1gina1&view=Formul%C3%A1rio`}
+          key={parameter}
+          src={`https://www.appsheet.com/start/e81f77d3-5b6c-4408-a07e-ae254943a988#appName=Galileu_backup-640720421&view=Formul%C3%A1rio&defaults=${parameter}`}
           title="External App"
           className="external-iframe"
           style={{
