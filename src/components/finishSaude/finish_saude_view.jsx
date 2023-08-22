83b36b6588a0001bde5e480cdf03f2a264ba4461
netomurilo13 <netomurilo100@gmail.com>
2023-08-22 12:40:25 -0300

    fix bug

--- src/components/finishSaude/finish_saude_view.jsx
+++ src/components/finishSaude/finish_saude_view.jsx
@@ -4,8 +4,8 @@ import "./index.css"
 
 const ThankYouPageSaude = () => {
   return (
-    <div className="thank-you-page">
-      <img src={logo} alt="Logo" className="logo" style={{ marginBottom: "100px" }} />
+    <div className="thank-you-page2">
+      <img src={logo} alt="Logo" className="logo2" style={{ marginBottom: "100px" }} />
       <h2>Obrigado por preencher o formulário!</h2>
       <p>Vamos avaliar as suas informações e logo logo você receberá uma mensagem com o resultado!</p>
     </div>
